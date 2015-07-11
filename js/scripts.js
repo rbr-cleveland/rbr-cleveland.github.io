e79398de3c833934d92815e47be56f3ec9ad5203
Richard Schulte <richard.j.schulte@gmail.com>
2015-07-11 09:18:49 -0400

    even more improvements

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,11 @@ jQuery(document).ready(function(){
 
 	}
 
+  $('.nav a').on('click', function(){
+    $(".btn-navbar").click(); //bootstrap 2.x
+    $(".navbar-toggle").click() //bootstrap 3.x by Richard
+  });
+
 	//Sticky Navigation
 
 	// jQuery("#navigation").sticky({topSpacing:1});
