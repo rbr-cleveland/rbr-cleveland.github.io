3c756d14201783aed605f4ee8d6f37dce0f0f163
Richard Schulte <richard.j.schulte@gmail.com>
2016-03-07 20:34:32 -0500

    fixed layouts and background

--- js/scripts.js
+++ js/scripts.js
@@ -128,99 +128,99 @@ jQuery(function() {
 // 	});
 // });
 
-// Parallax
-jQuery(window).bind('load', function () {
-	parallaxInit();
-});
+// // Parallax
+// jQuery(window).bind('load', function () {
+// 	parallaxInit();
+// });
 
-function parallaxInit() {
-    jQuery('.parallax').each(function(){
-        jQuery(this).parallax("30%", 0.1);
-    });
-}
+// function parallaxInit() {
+//     jQuery('.parallax').each(function(){
+//         jQuery(this).parallax("30%", 0.1);
+//     });
+// }
 
 // //Navigation Dropdown
 // jQuery('.nav a.collapse_menu1').click(function () { jQuery(".nav-collapse").collapse("hide") });
 jQuery('body').on('touchstart.dropdown', '.dropdown-menu', function (e) { e.stopPropagation(); });
 
-
-// Tabs
-jQuery(".tab_container").hide(); //Hide all content
-jQuery("ul.tabs li:first").addClass("tab-active").show(); //Activate first tab
-jQuery(".tab_container:first").show(); //Show first tab content
-
-jQuery("ul.tabs li").click(function() {
-		jQuery("ul.tabs li").removeClass("tab-active"); //Remove any "active" class
-		jQuery(this).addClass("tab-active"); //Add "active" class to selected tab
-		jQuery(".tab_container").hide(); //Hide all tab content
-		var activeTab = jQuery(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + content
-		jQuery(activeTab).fadeIn(); //Fade in the active content
-		return false;
-});
-// Toggle
-jQuery(".toggle_container").hide();
-jQuery("span.toggle-title").click(function(){
-		jQuery(this).toggleClass("toggle-active").next().slideToggle("normal");
-		return false;
-});
-jQuery(document).ready(function(){
-		//Elements Fading
-		jQuery('.element_from_top').each(function () {
-			jQuery(this).appear(function() {
-			  jQuery(this).delay(150).animate({opacity:1,top:"0px"},1000);
-			});
-		});
-
-		jQuery('.element_from_bottom').each(function () {
-			jQuery(this).appear(function() {
-			  jQuery(this).delay(150).animate({opacity:1,bottom:"0px"},1000);
-			});
-		});
-
-
-		jQuery('.element_from_left').each(function () {
-			jQuery(this).appear(function() {
-			  jQuery(this).delay(150).animate({opacity:1,left:"0px"},1000);
-			});
-		});
-
-
-		jQuery('.element_from_right').each(function () {
-			jQuery(this).appear(function() {
-			  jQuery(this).delay(150).animate({opacity:1,right:"0px"},1000);
-			});
-		});
-
-		jQuery('.element_fade_in').each(function () {
-			jQuery(this).appear(function() {
-			  jQuery(this).delay(150).animate({opacity:1,right:"0px"},1000);
-			});
-		});
-		//Animated Progress Bars
-		jQuery('.bar li').each(function () {
-			jQuery(this).appear(function() {
-			  jQuery(this).animate({opacity:1,left:"0px"},1200);
-			  var b = jQuery(this).find("span").attr("data-width");
-			  jQuery(this).find("span").animate({
-				width: b + "%"
-			  }, 1700, "easeOutCirc");
-			});
-		});
-		//Animated Counters
-		jQuery('.counters').each(function () {
-			jQuery(".timer .count").appear(function() {
-			var counter = jQuery(this).html();
-			jQuery(this).countTo({
-				from: 0,
-				to: counter,
-				speed: 2000,
-				refreshInterval: 60,
-				});
-			});
-		});
-
-		//Fading Out AlertBox
-		jQuery('.shortcode_alertbox').find('.box_close').click(function(){
-			jQuery(this).parents('.alertboxes').animate({opacity:0},300).animate({height:"0px"});
-		});
-});
+//
+// // Tabs
+// jQuery(".tab_container").hide(); //Hide all content
+// jQuery("ul.tabs li:first").addClass("tab-active").show(); //Activate first tab
+// jQuery(".tab_container:first").show(); //Show first tab content
+//
+// jQuery("ul.tabs li").click(function() {
+// 		jQuery("ul.tabs li").removeClass("tab-active"); //Remove any "active" class
+// 		jQuery(this).addClass("tab-active"); //Add "active" class to selected tab
+// 		jQuery(".tab_container").hide(); //Hide all tab content
+// 		var activeTab = jQuery(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + content
+// 		jQuery(activeTab).fadeIn(); //Fade in the active content
+// 		return false;
+// });
+// // Toggle
+// jQuery(".toggle_container").hide();
+// jQuery("span.toggle-title").click(function(){
+// 		jQuery(this).toggleClass("toggle-active").next().slideToggle("normal");
+// 		return false;
+// });
+// jQuery(document).ready(function(){
+// 		//Elements Fading
+// 		jQuery('.element_from_top').each(function () {
+// 			jQuery(this).appear(function() {
+// 			  jQuery(this).delay(150).animate({opacity:1,top:"0px"},1000);
+// 			});
+// 		});
+//
+// 		jQuery('.element_from_bottom').each(function () {
+// 			jQuery(this).appear(function() {
+// 			  jQuery(this).delay(150).animate({opacity:1,bottom:"0px"},1000);
+// 			});
+// 		});
+//
+//
+// 		jQuery('.element_from_left').each(function () {
+// 			jQuery(this).appear(function() {
+// 			  jQuery(this).delay(150).animate({opacity:1,left:"0px"},1000);
+// 			});
+// 		});
+//
+//
+// 		jQuery('.element_from_right').each(function () {
+// 			jQuery(this).appear(function() {
+// 			  jQuery(this).delay(150).animate({opacity:1,right:"0px"},1000);
+// 			});
+// 		});
+//
+// 		jQuery('.element_fade_in').each(function () {
+// 			jQuery(this).appear(function() {
+// 			  jQuery(this).delay(150).animate({opacity:1,right:"0px"},1000);
+// 			});
+// 		});
+// 		//Animated Progress Bars
+// 		jQuery('.bar li').each(function () {
+// 			jQuery(this).appear(function() {
+// 			  jQuery(this).animate({opacity:1,left:"0px"},1200);
+// 			  var b = jQuery(this).find("span").attr("data-width");
+// 			  jQuery(this).find("span").animate({
+// 				width: b + "%"
+// 			  }, 1700, "easeOutCirc");
+// 			});
+// 		});
+// 		//Animated Counters
+// 		jQuery('.counters').each(function () {
+// 			jQuery(".timer .count").appear(function() {
+// 			var counter = jQuery(this).html();
+// 			jQuery(this).countTo({
+// 				from: 0,
+// 				to: counter,
+// 				speed: 2000,
+// 				refreshInterval: 60,
+// 				});
+// 			});
+// 		});
+//
+// 		//Fading Out AlertBox
+// 		jQuery('.shortcode_alertbox').find('.box_close').click(function(){
+// 			jQuery(this).parents('.alertboxes').animate({opacity:0},300).animate({height:"0px"});
+// 		});
+// });
